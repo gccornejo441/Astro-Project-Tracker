a916a2daed07b310a2d68117bb2b31c73e23b692
unknown <gcornejo441@gmail.com>
2023-01-03 09:20:18 -0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), svelte(), tailwind()],
+});
