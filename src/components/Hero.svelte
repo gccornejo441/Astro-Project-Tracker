a651c60cf3cc59e9c491b554893dabb30c05420e
unknown <gcornejo441@gmail.com>
2023-01-10 15:03:19 -0800

    onmount fuction

--- src/components/Hero.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import { onDestroy, setContext } from 'svelte';
-	import { mapbox, key } from '.mapbox.js'
-</script>
-
---
+++ src/components/Out.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount } from "svelte";
+
+	let photos = []
+
+	onMount(async () => {
+		fetch("https://jsonplaceholder.typicode.com/photos")
+		.then((res) => res.json())
+		.then((data) => {
+			photos = data
+		})
+	
+	})
+
+
+</script>
+
+<div class="photos">
+	{#each photos as photo}
+	<figure>
+		<img src={photo.thumbnailUrl} alt={photo.title} />
+		<figcaption>{photo.title}</figcaption>
+	</figure>
+	{:else}
+	<p>...loading</p>
+	{/each}
+</div>
+
+
+<style>
+	.photos {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-gap: 8px;
+	}
+
+	figure, img {
+		width: 100%;
+		margin: 0;
+	}
+</style>
